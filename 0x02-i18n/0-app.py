3f3b5722f0a8e29de156a77de3123e2666c79d19
starlingvibes <starlingroot@protonmail.com>
2023-04-03 23:28:22 +0100

    updated file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
 """
-Using Flask-Babel to demonstrate internationalization
+Basic Flask app
 """
 
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """Return a string"""
     return render_template('0-index.html')
 
